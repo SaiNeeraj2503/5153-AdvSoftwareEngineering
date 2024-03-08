3c82b18e314b9a15e8a97317b6f122fce46f4483
Rakesh Rapalli <rakeshrapalli06@gmail.com>
2024-03-08 17:55:30 -0600

    login has been added to urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
