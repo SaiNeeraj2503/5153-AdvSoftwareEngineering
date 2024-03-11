dd7b1dafe2c32b8cc1c67021782c03df7685b3eb
SaiNeeraj2503 <neerajchandragiri888@gmail.com>
2024-03-10 20:34:53 -0500

    push

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,14 @@
-#create a model here
+# from django.db import models
+# import uuid
+
+# class User(models.Model):
+#     userid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     username = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         # Specify the MongoDB collection name
+#         db_table = 'users'
+# issue with commit, just for the test
