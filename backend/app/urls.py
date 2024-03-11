97161b26ba8100b10b9c71c31f7357433c85cfe4
Rakesh Rapalli <rakeshrapalli06@gmail.com>
2024-03-11 16:11:05 -0500

    Added paths for get and create posts

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
-    
+    path('create-post/', views.create_post, name='create-post'),
+    path('get-posts/', views.get_posts, name='get-posts'),
 ]
