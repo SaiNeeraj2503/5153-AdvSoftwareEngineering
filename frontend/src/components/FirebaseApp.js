651acf3317f7b82e53935e977e894ddcb9c01491
Rakesh Rapalli <rakeshrapalli06@gmail.com>
2024-03-18 22:04:37 -0500

    changes

--- frontend/src/components/FirebaseApp.js
+++ frontend/src/components/FirebaseApp.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
+	apiKey: "AIzaSyBmcz2hcsAtRD_NztXc2Iq09Etgza9GTLs",
+	authDomain: "mcc-e06c3.firebaseapp.com",
+	projectId: "mcc-e06c3",
+	storageBucket: "mcc-e06c3.appspot.com",
+	messagingSenderId: "145047817469",
+	appId: "1:145047817469:web:2ff747d98b05efb4abc294",
+	measurementId: "G-660THECH1T"
+  };
 
 const FirebaseApp = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 export  { FirebaseApp, firebaseConfig };
