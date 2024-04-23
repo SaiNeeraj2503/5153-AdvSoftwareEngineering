8b9c1ae5fe46e18d3b33ce810b23bde208c2aa7d
Rakesh Rapalli <rakeshrapalli06@gmail.com>
2024-04-23 13:21:28 -0500

    Updated Discover.js in the pages

--- frontend/src/pages/Discover.js
+++ frontend/src/pages/Discover.js
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
 import CreatePostModal from '../components/CreatePostModal';
 import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
-import {FirebaseApp, firebaseConfig} from '../components/FirebaseApp';
+import { v4 as uuidv4 } from 'uuid'; 
+import {FirebaseApp} from '../components/FirebaseApp';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'; 
-import InfiniteScrollComponent from '../components/InfiniteScrollComponent';
-
+import PostPagination from '../components/PostPagination';
 
 const storage = getStorage(FirebaseApp);
 
 
 const DiscoverPage = ({userId, email}) => {
   const [showCreatePostModal, setShowCreatePostModal] = useState(false);
+  const [creatingPost, setCreatingPost] = useState(false);
   const [postFormData, setPostFormData] = useState({
     title: '',
     content: '',
-    image: null, 
+    image: null,
   });
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleCreatePost = () => {
     setPostFormData({
@@ -24,7 +25,9 @@ const DiscoverPage = ({userId, email}) => {
       content: '',
       image: null,
     });
-    setShowCreatePostModal(true);
+    if (!creatingPost) {
+      setShowCreatePostModal(true);
+    }
   };
   
   const handleCloseModal = () => {
@@ -51,13 +54,18 @@ const DiscoverPage = ({userId, email}) => {
     }
   };
 
+  const handleSearchInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
 
   const handlePostSubmit = async (e) => {
     e.preventDefault();
-  
+    if (creatingPost) return;
     try {
+      setCreatingPost(true);
       const uniqueImageId = uuidv4();
-  
+
       const imageRef = ref(storage, `images/${uniqueImageId}`);
       await uploadBytes(imageRef, postFormData.image);
       var imageUrl
@@ -78,28 +86,29 @@ const DiscoverPage = ({userId, email}) => {
         timestamp: new Date(), 
       };
   
-      savePostToMongoDB(postData);
-  
+      await savePostToMongoDB(postData);
+
+      setCreatingPost(false);
+
       setPostFormData({
         title: '',
         content: '',
         image: null
       });
-  
 
       setShowCreatePostModal(false);
     } catch (error) {
       console.error('Error uploading image to Firebase Storage:', error);
+      setCreatingPost(false);
     }
   };
 
   const savePostToMongoDB = async (postData) => {
       try {
           const response = await axios.post('http://127.0.0.1:8000/app/create-post/', postData);
-          
+
           console.log(response.data.message);
       } catch (error) {
-          // Handle error
           console.error('Error saving post to MongoDB:', error);
       }
   };
@@ -107,13 +116,25 @@ const DiscoverPage = ({userId, email}) => {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex mb-8 justify-center">
-        <button className="bg-purple-700 text-white px-40 py-3 rounded-md hover:bg-purple-800" onClick={handleCreatePost}>
+        <button className="bg-purple-700 text-white px-40 py-3 rounded-md hover:bg-purple-800" onClick={handleCreatePost} disabled={creatingPost}>
           Create Post
         </button>
       </div>
 
-      <InfiniteScrollComponent email={email}/>
+      <div className="flex justify-center">
+        <input
+          type="text"
+          placeholder="Search posts..."
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+          className="block w-40 md:w-1/4 mx-2 mb-4 px-4 py-2 border border-gray-300 rounded-md"
+        />
+      </div>
 
+
+
+      <PostPagination email={email} query={searchQuery} creating={creatingPost}/>
+      
       <CreatePostModal
         show={showCreatePostModal}
         onClose={handleCloseModal}
